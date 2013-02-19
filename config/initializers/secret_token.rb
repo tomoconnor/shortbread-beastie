c21f36f531876ed09efb4f2ad492b70f853d0ed7
Tom O'Connor <tom@twinhelix.org>
2013-02-19 08:55:13 +0000

    adding a default rails app 'rails new shortbread-beastie'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShortbreadBeastie::Application.config.secret_token = '16073f1f45a09376dd4fedbe1308e2403af3263eede29465bc032c7986d39a08646b6f85ad6a395cc9ec8a939406460cfd9deee178e6eeb30fdee81d110e4a1e'
